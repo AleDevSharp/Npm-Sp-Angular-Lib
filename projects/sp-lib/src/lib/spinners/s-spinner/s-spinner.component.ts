b6d21651ea93b437a881d223ea21ce9674682800
DiscoHub12 <discordo12di@gmail.com>
2024-12-14 18:19:14 +0100

    + add new components

---
+++ projects/sp-lib/src/lib/spinners/s-spinner/s-spinner.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 's-spinner',
+  standalone: true,
+  imports: [],
+  templateUrl: './s-spinner.component.html',
+  styleUrl: './s-spinner.component.scss'
+})
+export class SSpinnerComponent {
+  // Constructor for SSpinnerOneComponent component
+  constructor() {}
+}
--- projects/sp-lib/src/public-api.ts
+++ projects/sp-lib/src/public-api.ts
@@ -4,3 +4,6 @@
 
 export * from './lib/btns/sp-btn-txt/sp-btn-txt.component';
 export * from './lib/btns/sp-btn/sp-btn.component';
+export * from './lib/dividers/s-divider/s-divider.component';
+export * from './lib/progressbars/s-progressbar/s-progressbar.component';
+export * from './lib/spinners/s-spinner/s-spinner.component';
